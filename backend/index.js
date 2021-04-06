4e316c2321f0195d0ab1baf6276dce61e6571849
boothscript <40050887+boothscript@users.noreply.github.com>
2021-04-06 08:49:08 +0100

    feat: :sparkles: add User model
    #3

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(3000, (err) => {
+app.listen(5000, (err) => {
   if (!err) {
-    console.log("Express sever running on port 3000");
+    console.log("Express sever running on port 5000");
   } else {
     throw new Error("ERROR: Failed to app.listen", err);
   }
